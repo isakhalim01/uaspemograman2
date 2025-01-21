9717c2db59ee5f88d88f3d1d82e78363850bb9a4
isakhalim01 <162536285+isakhalim01@users.noreply.github.com>
2025-01-21 10:11:32 +0700

    Add files via upload

---
+++ Materi5OverridingPadaInheritance/mobil.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Materi5OverridingPadaInheritance;
+
+/**
+ *
+ * @author ASUS
+ */
+public class mobil {
+    public void jalankan(){
+        System.out.println("Masukkan gigi 1, lepas kopling sambil tekan gas.....");
+    }
+}
